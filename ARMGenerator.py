d08dabe6620a07ce43325fd9b3d07f9ab31a46a9
Alejandro Tejada <tej17584@uvg.edu.gt>
2021-11-22 12:34:01 -0600

    cambios en el generador de ARM

--- ARMGenerator.py
+++ ARMGenerator.py
@@ -1,17 +1,12 @@
 class ARGCodigoGenerador:
 
     def __init__(self):
-        self.base = ""
+        self.valoresIniciales= ""
 
     def construirGOTO(self, etiqueta):
         # hay varios tipos de jumps
         retorno = f"j {etiqueta}"
         pass
-
-    def construirIf(self, Rsrc, etiqueta):
-
-        retorno = f"bgtz {Rsrc}, {etiqueta}"
-        return retorno
 
     def construirMultiplicacion(self, registros):
         return f"\tmul {registros[0]}, {registros[1]}, {registros[2]}\n"
@@ -22,48 +17,15 @@ class ARGCodigoGenerador:
     def construirSumaV2(self, registros, constante):
         return f"\tadd {registros[0]}, {registros[1]}, {constante}\n"
 
-    def cosntruirNegacion(self, Rdest, Rsrc1):
-        retorno = f"neg {Rdest}, {Rsrc1}"
-        pass
 
     def construirResta(self, registros):
         return f"\tsub {registros[0]}, {registros[1]}, {registros[2]}\n"
 
-    def construirNegacion(self, Rdest, Rsrc1):
-        retorno = f"not {Rdest}, {Rsrc1}"
-        pass
 
     def construirAnd(self, Rdest, Rsrc1, Rsrc2):
         retorno = f"and {Rdest}, {Rsrc1}, {Rsrc2}"
         pass
 
-    def construirOr(self, Rdest, Rsrc1, Rsrc2):
-        retorno = f"or {Rdest}, {Rsrc1}, {Rsrc2}"
-        pass
-
-    def construirComparacionIgual(self, Rdest, Rsrc1, Rsrc2):
-        retorno = f"seq {Rdest}, {Rsrc1}, {Rsrc2}"
-        pass
-
-    def construirComparacionMayorIgual(self, Rdest, Rsrc1, Rsrc2):
-        retorno = f"sge {Rdest}, {Rsrc1}, {Rsrc2}"
-        pass
-
-    def construirComparacionMayor(self, Rdest, Rsrc1, Rsrc2):
-        retorno = f"sgt {Rdest}, {Rsrc1}, {Rsrc2}"
-        pass
-
-    def construirComparacionMenorIgual(self, Rdest, Rsrc1, Rsrc2):
-        retorno = f"sle {Rdest}, {Rsrc1}, {Rsrc2}"
-        pass
-
-    def construirComparacionMenor(self, Rdest, Rsrc1, Rsrc2):
-        retorno = f"slt {Rdest}, {Rsrc1}, {Rsrc2}"
-        pass
-
-    def construirComparacionNoIgual(self, Rdest, Rsrc1, Rsrc2):
-        retorno = f"sne {Rdest}, {Rsrc1}, {Rsrc2}"
-        pass
 
     def construirEncabezado(self, nombreHeader):
         return(f'''
